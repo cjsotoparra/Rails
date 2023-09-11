7a37e93207423a71c10a6018b9173c3410ed7389
cjsotoparra <christian.j.sotoparra@gmail.com>
2023-09-10 21:33:07 -0700

    Edit and Update

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -23,4 +23,18 @@ class ArticlesController < ApplicationController
 			render 'new', status: :unprocessable_entity
 		end
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article was updated sucessfully"
+			redirect_to @article
+		else
+			render 'edit', status: :unprocessable_entity
+		end
+	end
 end
