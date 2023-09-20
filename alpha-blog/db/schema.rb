9a8cd1986161d58d5ff22753f5e93b10cd497f48
cjsotoparra <christian.j.sotoparra@gmail.com>
2023-09-20 00:21:17 -0700

    Add one-to-many assocation between users and articles from backend

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_062143) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_070544) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "create_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
