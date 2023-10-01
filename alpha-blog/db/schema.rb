e5c58e732d840904feb662cd9d0acfba7e3bb71e
cjsotoparra <christian.j.sotoparra@gmail.com>
2023-10-01 01:51:20 -0700

    Admin user

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_050822) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_084559) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_050822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
